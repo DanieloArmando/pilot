97defda1fe8bfa8d3b6edc3cfb524adc76c8b88c
Daniel Vaknine <daniel.vaknine@gmail.com>
2018-11-19 14:36:23 +0100

    fourth

--- src/store.js
+++ src/store.js
@@ -5,11 +5,18 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    count: 0
+    count: 0,
+    users: []
   },
   mutations: {
     add(state) {
       state.count++;
+    },
+    remove(state, item) {
+      state.count = item;
+    },
+    addUser(state, user) {
+      state.users.push(user);
     }
   },
   actions: {
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,9 +1,20 @@
 <template>
 <div>
+
+  <div class="about">
+    <h1>New User</h1>
+    <input v-on:enter="addUser" v-model="str" placerholder="..."> </input>
+    <button v-on:click="addUser">Add</button>
+  </div>
+  <div class="user" v-for="(user,index) in users">
+    <h1> {{user}}</h1>
+    <h2>{{user}}</h2>
+  </div>
   <div class="about" v-for="(item,index) in items">
     <h1> {{item}}</h1>
     <h2>{{item}}</h2>
-    <button v-on:click="">{{index}}</button>
+        <button v-on:click="count(item)">{{index}}</button>
+
   </div>
 </div>
 </template>
@@ -12,8 +23,22 @@
 export default {
   data: function() {
     return {
-      items: ["hello", "world", "yo"]
+      items: ["hello", "world", "yo"],
+      str: "",
+      users: this.$store.state.users
     };
+  },
+  methods: {
+    count(item) {
+      this.$store.commit("add");
+      this.$store.commit("remove", item);
+      console.log(item + "" + this.$store.state.count);
+    },
+    addUser() {
+      console.log("Adding User," + this.str);
+      this.$store.commit("addUser", this.str);
+      console.log(this.$store.state.users);
+    }
   }
 };
 </script>
@@ -21,7 +46,7 @@ export default {
 <style lang="scss">
 $bgColor: rgb(30, 35, 44);
 $btColor: rgb(58, 88, 145);
-.about {
+%frame {
   background-color: $bgColor;
 
   color: white;
@@ -38,9 +63,25 @@ $btColor: rgb(58, 88, 145);
     background-color: $btColor;
     color: white;
     font-size: 200%;
-    width: 50%;
+    width: 400px;
+    padding: 20px;
     border-radius: 10px;
   }
+  input {
+    width: 300px;
+    height: 20px;
+    text-align: center;
+    font-size: 20px;
+    padding: 20px;
+    border-top-left-radius: 10px;
+    border-top-right-radius: 10px;
+  }
+}
+.about {
+  @extend %frame;
+}
+.user {
+  @extend %frame;
 }
 </style>
 
