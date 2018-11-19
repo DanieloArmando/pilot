1c38af94ee28b509b6bad520badf6f06ef88c3da
Daniel Vaknine <danielvaknine@Daniels-MacBook-Pro.local>
2018-11-19 13:31:42 +0100

    first

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,44 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+<div>
+  <div class="about" v-for="(item,index) in items">
+    <h1> {{index}}</h1>
+    <h2>{{item}}</h2>
+    <button>Add</button>
   </div>
+</div>
 </template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      items: ["hello", "world", "yo"]
+    };
+  }
+};
+</script>
+
+<style lang="scss">
+$bgColor: rgb(30, 35, 44);
+$btColor: rgb(58, 88, 145);
+.about {
+  background-color: $bgColor;
+
+  color: white;
+  min-height: 400px;
+  padding: 20px;
+
+  h1 {
+    font-weight: 500;
+  }
+  h2 {
+    font-weight: 100;
+  }
+  button {
+    background-color: $btColor;
+    color: white;
+    font-weight: 200%;
+  }
+}
+</style>
+
