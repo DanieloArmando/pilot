135f82aead4ba3b75e6eaa999529ddc393e0661d
armando von gohren antequera <armandovongohrenantequera@armandos-MacBook-Pro.local>
2018-11-20 11:50:18 +0100

    sixth

--- src/store.js
+++ src/store.js
@@ -5,14 +5,21 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    count: 0
+    users: [
+      {name: 'Armando', password: 'password'},
+      {name: 'Daniel', password: 'password'}
+    ]
   },
   mutations: {
     add(state) {
       state.count++;
+    },
+    changeName: (state, [index, name]) => {
+      state.users[index].name = name;
+      localStorage.setItem('users', JSON.stringify(state.users))
+    },
+    loadUsers: (state) => {
+      console.log(JSON.parse(localStorage.getItem('users')))
     }
-  },
-  actions: {
-
   }
 })
---
+++ src/views/users/EditUser.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="row justify-content-center">
+        <div class="col-md-4 col-md-offset-4">
+            <button class="btn btn-primary" @click="loadUsers"></button>
+            <div class="form-group">
+                <label for="edit_name">Name</label>
+                <input 
+                    type="text" 
+                    class="form-control" 
+                    id="edit_name" 
+                    v-model="user.name" 
+                    @keydown.enter="changeName([id, user.name])">
+            </div>
+            {{$store.state.users}}
+            {{$route.params.user}}
+            {{user.name}}
+            {{$store.state.count}}
+        </div>
+    </div>
+</template>
+
+<script>
+import { mapMutations } from 'vuex'
+
+export default {
+    data() {
+        return {
+            id: this.$route.params.id,
+            user: this.$route.params.user
+        }
+    },
+    methods: {
+        ...mapMutations([
+            'changeName',
+            'loadUsers'
+        ])
+    }
+}
+</script>
